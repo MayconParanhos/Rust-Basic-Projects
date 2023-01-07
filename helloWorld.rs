f17416c8b4024cd926e475a51c444f2840507274
Maycon Paranhos <maycon.rodrigues1@gmail.com>
2023-01-07 18:53:18 -0300

    My hello world in Rust

---
+++ helloWorld.rs
@@ -0,0 +1,7 @@
+// This is my first hello world in Rust
+
+fn main(){
+    const STR: &str = "Hello World!";
+    let str2: &str = "My name is Maycon and I want to become fluent in Rust.";
+    println!("\n\n{}\n{}\n", STR, str2);
+}
